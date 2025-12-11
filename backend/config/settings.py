e2bb855ca778deb8dd62851f3c2a882586c5965c
Sidhesh <55739731+Sidhesh-Sherlekar@users.noreply.github.com>
2025-12-11 14:59:57 +0530

    Fix ALLOWED_HOSTS default value in settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-w2ceo7==+%$mv1(=^zjm*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,testserver,cipla-dms.onrender.com/').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,testserver,cipla-dms.onrender.com').split(',')
 
 # Application definition
 INSTALLED_APPS = [
